ab4f6869ff93f310c1c3d744ba3669d287a18125
Rowan Zellers <rowanz@cs.washington.edu>
2017-11-16 11:46:48 -0800

    new setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 # Contains all the commands used for setup for posterity
-
+# First you need to download and install python3.5.2
 conda install virtualenv
 virtualenv venv --no-site-packages
 source venv/bin/activate
+# For CPU
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.11.0-py3-none-any.whl
+pip install --upgrade $TF_BINARY_URL
 pip install -r requirements.txt
+
+
 echo "Now download save.zip from https://worksheets.codalab.org/bundles/0xbe46c9b116fa443fb5cb743ab9fa4104/"
 echo "Download glove vectors from https://worksheets.codalab.org/bundles/0x15a09c8f74f94a20bec0b68a2e6703b3/"
